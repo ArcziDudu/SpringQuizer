d0acd57e4507c8dbaeed868a533793ce662ba3a3
arcziDudu <artur.augustyn26@gmail.com>
2023-10-15 15:17:21 +0200

    scoreboard and game info per user rest api endpoint added

--- src/main/java/com/quizer/api/controller/exception/GlobalExceptionRestHandler.java
+++ src/main/java/com/quizer/api/controller/exception/GlobalExceptionRestHandler.java
@@ -8,6 +8,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -38,5 +39,13 @@ public class GlobalExceptionRestHandler extends ResponseEntityExceptionHandler {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(responseBody);
     }
-
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> handleUsernameNotFoundException(UsernameNotFoundException ex) {
+        String errorMessage = ex.getMessage();
+        String responseBody = "{\"statusCode\": \"NotFound\", \"error\": \"" + errorMessage + "\"}";
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(responseBody);
+    }
 }
